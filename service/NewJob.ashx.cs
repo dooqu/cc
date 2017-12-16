79f4f295ceb98a9ebbcc3ff398082d8ebbba097f
dooqu <51045352@qq.com>
2017-12-16 22:23:30 +0800

    handler中增加创建工单请求

--- service/NewJob.ashx.cs
+++ service/NewJob.ashx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Web;
 using callcenter.modal;
+using callcenter.bll;
 
 namespace callcenter.service
 {
@@ -10,9 +11,99 @@ namespace callcenter.service
     /// </summary>
     public class NewJob : baseHandler.WechatServiceHandler
     {
+        public class NewJobResponseInfo : ResponseInfo
+        {
+            public int JobId
+            {
+                get;set;
+            }
+
+            public NewJobResponseInfo(int jobId)
+            {
+                this.JobId = jobId;
+            }
+        }
         public override ResponseInfo OnLoad(HttpContext context)
         {
-            throw new NotImplementedException();
+            if(CurrentSession == null)
+            {
+                return new ResponseInfo(true, "登录状态已经过期");
+            }
+
+            if(CheckRequestParams(context, "name", "mobile", "idcard", "channel", "comment", "type") == false)
+            {
+                return new ResponseInfo(true, "参数不完整");
+            }
+
+            int type = 0;
+            if(Int32.TryParse(GetParam("type"), out type) == false || type < 0 || type > 3)
+            {
+                return new ResponseInfo(true, "类型错误");
+            }
+
+            JobInfo job = new JobInfo();
+            job.UserId = CurrentSession.UserId;
+            job.JobType = type;
+
+            if(type == 0)
+            {
+                if(CheckRequestParams(context, "area", "address") == false)
+                {
+                    return new ResponseInfo(true, "邮寄地址错误");
+                }
+                else
+                {
+                    job.Area = GetParam("area");
+                    job.Address = GetParam("address");
+                }
+            }
+            else if(type == 1)
+            {
+                int changeType = 0;
+                if(CheckRequestParams(context, "changeType", "oldMobile", "newMobile") == false || Int32.TryParse(GetParam("changeType"), out changeType) == false || changeType <0 || changeType > 1)
+                {
+                    return new ResponseInfo(true, "手机号相关信息错误");
+                }
+                else
+                {
+                    job.MobileChangeType = changeType;
+                    job.MobilePhoneOld = GetParam("oldMobile");
+                    job.MobilePhoneNew = GetParam("newMobile");
+                }
+            }
+            else if(type == 2)
+            {
+                if(CheckRequestParams(context, "oldCard", "newCard") == false)
+                {
+                    return new ResponseInfo(true, "卡号相关信息错误");
+                }
+                else
+                {
+                    job.OldBankcard = GetParam("oldCard");
+                    job.NewBankcard = GetParam("newCard");
+                }
+            }
+            else if(type == 3)
+            {
+
+            }
+                        
+            job.UserName = GetParam("name");
+            job.MobilePhone = GetParam("mobile");
+            job.IdentityCard = GetParam("idcard");
+            job.ChannelCustomer = GetParam("channel");
+            job.CustomerRemark = GetParam("comment");
+
+            int jobId = Jobs.NewJob(job);
+
+            if(jobId > 0)
+            {
+                return new NewJobResponseInfo(jobId);
+            }
+            else
+            {
+                return new ResponseInfo(true, "创建工单失败");
+            }
         }
     }
 }
--- service/UpdateJob.ashx.cs
+++ service/UpdateJob.ashx.cs
@@ -1,26 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Web;
+using callcenter.modal;
+using callcenter.bll;
 
 namespace callcenter.service
 {
     /// <summary>
     /// UpdateJob 的摘要说明
     /// </summary>
-    public class UpdateJob : IHttpHandler
+    public class UpdateJob : baseHandler.WechatServiceHandler
     {
-
-        public void ProcessRequest(HttpContext context)
+        public override ResponseInfo OnLoad(HttpContext context)
         {
-            context.Response.ContentType = "text/plain";
-            context.Response.Write("Hello World");
-        }
-
-        public bool IsReusable
-        {
-            get
+            if(CurrentSession == null)
             {
-                return false;
+                return new ResponseInfo(true, "登录状态已经过期");
+            }
+
+            if (CheckRequestParams(context, "jobId", "imageIndex") == false)
+            {
+                return new ResponseInfo(true, "参数缺失");
+            }
+
+            int jobId, imageIndex = 0;
+
+            if(Int32.TryParse(GetParam("jobId"), out jobId) == false)
+            {
+                return new ResponseInfo(true, "jobid错误");
+            }
+
+            if(Int32.TryParse(GetParam("imageIndex"), out imageIndex) == false ||imageIndex < 1 || imageIndex > 5 )
+            {
+                return new ResponseInfo(true, "imageIndex错误");
+            }
+
+            HttpPostedFile imageFile = context.Request.Files["imageFile"];
+
+            if(imageFile == null)
+            {
+                return new ResponseInfo(true, "uploadimage缺失");
+            }
+
+            string imageNewName = Guid.NewGuid().ToString("n") + System.IO.Path.GetExtension(imageFile.FileName);
+            imageFile.SaveAs(System.IO.Path.Combine(context.Server.MapPath("/pics"), imageNewName));
+
+            if(Jobs.UpdateJob(jobId, imageIndex, imageNewName) >= 0)
+            {
+                return new ResponseInfo(false, "操作成功");
+            }
+            else
+            {
+                return new ResponseInfo(true, "上传失败");
             }
         }
     }
