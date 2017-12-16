db9f2da86077dc9936f25d8f99958e916754a76b
dooqu <51045352@qq.com>
2017-12-16 16:20:08 +0800

    同步modal

--- modal/JobInfo.cs
+++ modal/JobInfo.cs
@@ -6,52 +6,6 @@ namespace callcenter.modal
 {
     public class JobInfo
     {
-<<<<<<< HEAD
-        public int JobId { get; set; }
-
-        //客户的id
-        public int UserId
-        {
-            get;set;
-        }
-
-        //客户姓名
-        public string UserName
-        { get; set; }
-
-        //手机号
-        public string MobilePhone { get; set; }
-
-        public string ChannelCustomer { get; set; }
-
-        //客户备注
-        public string CustomerRemark { get; set; }
-
-        //状态(0待受理， 1受理，2 完成，3错误)
-        public int Status { get; set; }
-
-
-        //优先级(0一般、1 加急、2 紧急)
-        public int Priority { get; set; }
-
-        //工单被打回的错误结论
-        public string ErrorMessage
-        {
-            get; set;
-        }
-
-
-        //处理人
-        public OperatorInfo Operator { get; set; }
-
-
-        //处理时间
-        public DateTime OperationDateTime { get; set; }
-
-        //创建时间
-        public DateTime CreateDateTime { get; set; }
-        //是否删除 0 否 1 是
-=======
         #region 公共属性
         public int ID { get; set; }
         //UserId
@@ -126,7 +80,5 @@ namespace callcenter.modal
         //图片5
         public string Image5 { get; set; }
         #endregion
-
->>>>>>> 51ce6e033efa369ebc966dec784e9dd84d71c663
     }
 }
