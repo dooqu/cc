54d1a86e93b2d0bc7d82a98c4d1682f1941cacd6
fyh60318647 <fyh60318647@126.com>
2017-12-16 18:46:28 +0800

    修改添加工单逻辑2017/12/16 18

--- dal/DataProvider.cs
+++ dal/DataProvider.cs
@@ -48,6 +48,31 @@ namespace callcenter.dal
         {
             return Convert.ToInt32(SqlHelper.ExecuteScalar(ConfigurationManager.ConnectionStrings["sqlconn"].ConnectionString, "[AddJobInfo]",
                 new SqlParameter("@UserId", ci.UserId),
+                new SqlParameter("@JobType", ci.JobType),
+                new SqlParameter("@UserName", ci.UserName),
+                new SqlParameter("@IdentityCard", ci.IdentityCard),
+                new SqlParameter("@MobilePhone", ci.MobilePhone),
+                new SqlParameter("@ChannelCustomer", ci.ChannelCustomer),
+                new SqlParameter("@CustomerRemark", ci.CustomerRemark),
+                new SqlParameter("@Status", ci.Status),
+                new SqlParameter("@ErrorMessage", ci.ErrorMessage),
+                new SqlParameter("@Operation", ci.Operation),
+                new SqlParameter("@OperationDateTime", ci.OperationDateTime),
+                new SqlParameter("@CreateDateTime", ci.CreateDateTime),
+                new SqlParameter("@IsDelete", ci.IsDelete),
+
+                new SqlParameter("@Area", ci.Area),
+                new SqlParameter("@Address", ci.Address),
+                new SqlParameter("@ExpressNumber", ci.ExpressNumber),
+                new SqlParameter("@DueBillNumber", ci.DueBillNumber),
+                new SqlParameter("@ClearanceImage", ci.ClearanceImage),
+
+                new SqlParameter("@MobileChangeType", ci.MobileChangeType),
+                new SqlParameter("@MobilePhoneOld", ci.MobilePhoneOld),
+                new SqlParameter("@MobilePhoneNew", ci.MobilePhoneNew),
+
+                new SqlParameter("@OldBankcard", ci.OldBankcard),
+                new SqlParameter("@NewBankcard", ci.NewBankcard),
 
                 new SqlParameter("@Image1", ci.Image1),
                 new SqlParameter("@Image2", ci.Image2),
@@ -124,19 +149,20 @@ namespace callcenter.dal
                 cf.OperationDateTime = Convert.ToDateTime(reader["OperationDateTime"]);
                 cf.CreateDateTime = Convert.ToDateTime(reader["CreateDateTime"]);
                 cf.IsDelete = Convert.ToInt32(reader["IsDelete"]);
-                ci.Area=Convert.ToString(reader["Area"]);
-                ci.Address = Convert.ToString(reader["Address"]);
-                ci.ExpressNumber = Convert.ToString(reader["ExpressNumber"]);
-                ci.DueBillNumber = Convert.ToString(reader["DueBillNumber"]);
-                ci.ClearanceImage = Convert.ToString(reader["ClearanceImage"]);
+
+                ci.Area = reader["Area"] == null ? "" : Convert.ToString(reader["Area"]);
+                ci.Address = reader["Address"] == null ? "" : Convert.ToString(reader["Address"]);
+                ci.ExpressNumber = reader["ExpressNumber"] == null ? "" : Convert.ToString(reader["ExpressNumber"]);
+                ci.DueBillNumber = reader["DueBillNumber"] == null ? "" : Convert.ToString(reader["DueBillNumber"]);
+                ci.ClearanceImage = reader["ClearanceImage"] == null ? "" : Convert.ToString(reader["ClearanceImage"]);
 
                 ci.MobileChangeType = Convert.ToInt32(reader["MobileChangeType"]);
-                ci.MobilePhoneOld = Convert.ToString(reader["MobilePhoneOld"]);
-                ci.MobilePhoneNew = Convert.ToString(reader["MobilePhoneNew"]);
+                ci.MobilePhoneOld = reader["MobilePhoneOld"] == null ? "" : Convert.ToString(reader["MobilePhoneOld"]);
+                ci.MobilePhoneNew = reader["MobilePhoneNew"] == null ? "" : Convert.ToString(reader["MobilePhoneNew"]);
 
-                ci.OldBankcard = Convert.ToString(reader["OldBankcard"]);
-                ci.NewBankcard = Convert.ToString(reader["NewBankcard"]);
-                
+                ci.OldBankcard = reader["OldBankcard"] == null ? "" : Convert.ToString(reader["OldBankcard"]);
+                ci.NewBankcard = reader["NewBankcard"] == null ? "" : Convert.ToString(reader["NewBankcard"]);
+
 
                 cInfoList.Add(cf);
             }
