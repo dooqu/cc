3d56ea18ed733788842f50e591fa947059b9b9e6
dooqu <51045352@qq.com>
2017-12-22 00:32:34 +0800

    增加广告接口

---
+++ modal/AdInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace callcenter.modal
+{
+    public class AdInfo
+    {
+        public string ImagePath
+        {
+            get; set;
+        }
+
+        public int AdId
+        {
+            get; set;
+        }
+    }
+}
---
+++ service/GetAds.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="GetAds.ashx.cs" Class="callcenter.service.GetAds" %>
